287845452733c6995f684da227b2c866e89d2d6b
ttluattlua <ttluattlua@gmail.com>
2018-04-02 11:03:58 +0900

    버거벅스 어드민
    버거벅스 어드민

---
+++ BurgerBucks Admin/src/bba/com/a/controller/BbaMemberController.java
@@ -0,0 +1,75 @@
+package bba.com.a.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import bba.com.a.service.BbaMemberSerivce;
+
+@Controller
+public class BbaMemberController {
+	private static final Logger logger = LoggerFactory.getLogger(BbaMemberController.class);
+	
+	@Autowired
+	BbaMemberSerivce bbMemberService;
+	
+	/*--------------------------------------------------------------------------------------------
+	 * 로그인후 메인
+	 *-------------------------------------------------------------------------------------------*/
+	@RequestMapping(value="login.do", method=RequestMethod.GET)
+	public String login(Model model) {
+		logger.info("BbaMemberController login");
+		return "home.tiles";
+	}
+	
+	/*--------------------------------------------------------------------------------------------
+	 * 사원등록으로 이동 
+	 *-------------------------------------------------------------------------------------------*/
+	@RequestMapping(value="adminplus.do", method=RequestMethod.GET)
+	public String adminplus(Model model) {
+		logger.info("BbaMemberController adminplus");
+		return "adminplus.tiles";
+	}
+	
+	/*--------------------------------------------------------------------------------------------
+	 * 지점관리자로 이동 
+	 *-------------------------------------------------------------------------------------------*/
+	@RequestMapping(value="adminlist.do", method=RequestMethod.GET)
+	public String adminlist(Model model) {
+		logger.info("BbaMemberController adminlist");
+		return "adminlist.tiles";
+	}
+	
+	
+	/*--------------------------------------------------------------------------------------------
+	 * 고객관리로 이동 
+	 *-------------------------------------------------------------------------------------------*/
+	@RequestMapping(value="customerlist.do", method=RequestMethod.GET)
+	public String customerlist(Model model) {
+		logger.info("BbaMemberController customerlist");
+		return "customerlist.tiles";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
